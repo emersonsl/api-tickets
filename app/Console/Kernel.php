17734a5db623e86ad25aee7e173d3cbf692cb1dd
emersonsl <emerson.sl.1605@gmail.com>
2024-02-29 07:07:48 -0300

    feat: remove expired token

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,6 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
+        $schedule->command('auth:clear-resets')->everyFifteenMinutes();
         // $schedule->command('inspire')->hourly();
     }
 
